ef3c335ce2cc4d966c3bc1f0202f66a45ffd2f05
Kaan Tasbas <mktasbas@gmail.om>
2019-03-05 17:51:21 -0500

    made .gitignore for build/test directories and manually removed files

--- gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/kaan/Documents/src/ed2/catkin_ws/src/cv_pipeline/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
